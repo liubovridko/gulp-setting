0e3e470716093bcea762c772db20e094268deb95
Liubov <liubovusikova1@gmail.com>
2023-08-25 20:19:36 +0300

    Init

---
+++ gulp/config/plugins.js
@@ -0,0 +1,7 @@
+import replace from "gulp-replace";
+import plumber from "gulp-plumber"; //resolve error
+import notify from "gulp-notify"; //output message error
+
+export const plugins = {
+	replace: replace,
+};
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { plugins } from "./gulp/config/plugins.js";
+
+global.app = {
+	path: path,
+	gulp: gulp,
+	plugins: plugins,
+};
+
+function watcher() {
+	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html);
+}
+const mainTasks = gulp.parallel(copy, html);
+const dev = gulp.series(reset, mainTasks, watcher);
+
+gulp.task("default", dev);
